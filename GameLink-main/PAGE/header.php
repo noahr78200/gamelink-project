827118c92c8d6bf844a11696f4b8c9077f898e6b
Noah <noahron78200@icloud.com>
2025-11-13 14:40:24 +0100

    connexion captcha avec l'admin

--- GameLink-main/PAGE/header.php
+++ GameLink-main/PAGE/header.php
@@ -15,7 +15,7 @@ require_once __DIR__ . '/../INCLUDES/check_admin.php';
 <header>
     <nav class="Menu">
         <a href="">
-            <img class="logo" src="/GameLink/ICON/LogoComplet.svg" alt="Logo GameLink" width="">
+            <img class="logo" src="../ICON/LogoComplet.svg" alt="Logo GameLink" width="">
         </a>
         <a href="ACCUEIL.php">ACCUEIL</a>
         <a href="RECHERCHE.php">RECHERCHE</a>
@@ -27,6 +27,6 @@ require_once __DIR__ . '/../INCLUDES/check_admin.php';
         <?php endif; ?>
     </nav>
     <a href="">
-        <img src="/GameLink/ICON/iconProfil.svg" alt="Logo Profil" width="">
+        <img src="../ICON/iconProfil.svg" alt="Logo Profil" width="">
     </a>
 </header>
