28a54735abbac10fcb92f3a5dc48d6fc4f055899
Noah <noahron78200@icloud.com>
2025-11-25 11:43:45 +0100

    page chat

--- GameLink-main/PAGE/CHAT.php
+++ GameLink-main/PAGE/CHAT.php
@@ -1,1 +1,16 @@
+
+<DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="GameLink - Votre plateforme de jeux vidéo">
+    <title>GameLink - Chat</title>
+    <link rel="stylesheet" href="../CSS/HEADER.css" type="text/css"/>
+    <link rel="stylesheet" href="../CSS/STYLE_CHAT.css" type="text/css"/>
+    <link rel="icon" type="image/svg+xml" href="../ICON/LogoSimple.svg">
+</head>
+<?php
+    include '../INCLUDES/HEADER.php';   
+?>
 <?php echo 'Lyes doit finir la page profil avant de commencer CHAT.php'; exit; ?>
