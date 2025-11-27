b51cf19fc55d9ecf779452915712f410047c5dba
Noah <noahron78200@icloud.com>
2025-11-27 10:09:05 +0100

    page index

--- GameLink-main/index.php
+++ GameLink-main/index.php
@@ -13,7 +13,7 @@ unset($_SESSION['flash_index']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="GameLink - Votre plateforme de jeux vidéo">
     <title>GameLink - Accueil</title>
-    <link rel="stylesheet" href="CSS/STYLE_INDEX_NEW.css" type="text/css"/>
+    <link rel="stylesheet" href="CSS/STYLE_INDEX.css" type="text/css"/>
     <link rel="icon" type="image/svg+xml" href="ICON/LogoSimple.svg">
 </head>
 <body>
